b5c458745b6051e4d81da55eec08d25d54dabb8c
Han <Han@Han-PC>
2016-06-26 17:20:36 +0900

    java basic p272

--- JavaStudy/src/ch7/SingletonTest.java
+++ JavaStudy/src/ch7/SingletonTest.java
@@ -17,7 +17,7 @@ final class Singleton{
 
 public class SingletonTest {
 	public static void main(String[] args) {
-		//Singleton s = new Singleton();
+//		Singleton s = new Singleton();
 		Singleton s1 = Singleton.getInstance();
 		System.out.println("s1==="+s1);
 	}
